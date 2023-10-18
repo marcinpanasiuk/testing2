67b92d232f7d0c052f12383222fce12838c88047
marcinpanasiuk <93267852+marcinpanasiuk@users.noreply.github.com>
2023-10-18 10:36:52 +0200

    Update autorun.js

--- autorun.js
+++ autorun.js
@@ -7,12 +7,12 @@ function onNewMessageComposeHandler(event) {
     var xmlhttp = new XMLHttpRequest();
     
     xmlhttp.onload = function() {
-        console.error(xmlhttp.responseText);
         var signature = `<strong style='font-size: 20px; color: green'>Success: ${xmlhttp.responseText} </strong>`;
         Office.context.mailbox.item.body.setSignatureAsync(signature, { coercionType: "html" }, function () { event.completed(); });
     }
     
     xmlhttp.onerror = function() {
+        console.error(xmlhttp.responseText);
         var signature = `<strong style='font-size: 20px; color: red'>Error: ${xmlhttp.responseText} </strong>`;
         Office.context.mailbox.item.body.setSignatureAsync(signature, { coercionType: "html" }, function () { event.completed(); });
     }
