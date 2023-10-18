26dcfa5a89385fe1e39babbb55c58faae7badc66
marcinpanasiuk <93267852+marcinpanasiuk@users.noreply.github.com>
2023-10-18 08:32:39 +0200

    Update autorun.js

--- autorun.js
+++ autorun.js
@@ -7,6 +7,7 @@ function onNewMessageComposeHandler(event) {
     var xmlhttp = new XMLHttpRequest();
     
     xmlhttp.onload = function() {
+        console.error(xmlhttp.responseText);
         var signature = `<strong style='font-size: 20px; font-color: greeen'>Success: ${xmlhttp.responseText} </strong>`;
         Office.context.mailbox.item.body.setSignatureAsync(signature, { coercionType: "html" }, function () { event.completed(); });
     }
@@ -15,9 +16,16 @@ function onNewMessageComposeHandler(event) {
         var signature = `<strong style='font-size: 20px; font-color: red'>Error: ${xmlhttp.responseText} </strong>`;
         Office.context.mailbox.item.body.setSignatureAsync(signature, { coercionType: "html" }, function () { event.completed(); });
     }
-    
-    xmlhttp.open('GET', 'https://marcinpanasiuk.github.io/testing2/api/get', true);    
-    xmlhttp.send();
+
+    try {
+        xmlhttp.open('GET', 'https://marcinpanasiuk.github.io/testing2/api/get', true);    
+        xmlhttp.send();
+    }
+    catch(error) {
+        console.error(error);
+        var signature = `<strong style='font-size: 20px; font-color: red'>Error: ${error} </strong>`;
+        Office.context.mailbox.item.body.setSignatureAsync(signature, { coercionType: "html" }, function () { event.completed(); });
+    }
 }
 
 Office.actions.associate("onNewMessageComposeHandler", onNewMessageComposeHandler);
