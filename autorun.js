fade0872e51a7d846a2e5fe0905a2e289904b689
marcinpanasiuk <93267852+marcinpanasiuk@users.noreply.github.com>
2023-10-18 08:15:24 +0200

    Update autorun.js

--- autorun.js
+++ autorun.js
@@ -16,7 +16,7 @@ function onNewMessageComposeHandler(event) {
         Office.context.mailbox.item.body.setSignatureAsync(signature, { coercionType: "html" }, function () { event.completed(); });
     }
     
-    xmlhttp.open('GET', 'api/get', true);    
+    xmlhttp.open('GET', 'https://marcinpanasiuk.github.io/testing2/api/get', true);    
     xmlhttp.send();
 }
 
