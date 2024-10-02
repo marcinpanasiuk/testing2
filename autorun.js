7fae20cdc8bc8bc70764ef9245d5485696a95339
marcinpanasiuk <93267852+marcinpanasiuk@users.noreply.github.com>
2024-10-02 16:48:09 +0200

    typo fix

--- autorun.js
+++ autorun.js
@@ -8,7 +8,7 @@ function onNewMessageComposeHandler(event) {
         .catch(function (e) { result = `Fail: ${e.message}`; })
         .finally(function () { 
             var endTime = performance.now();
-            Office.context.mailbox.item.body.prependAsync(`${result}, it took `${Math.round(endTime - startTime)} ms``, { coercionType: "html" }, function () { event.completed(); }) });
+            Office.context.mailbox.item.body.prependAsync(`${result}, it took ${Math.round(endTime - startTime)} ms`, { coercionType: "html" }, function () { event.completed(); }) });
 }
 
 Office.actions?.associate("onNewMessageComposeHandler", onNewMessageComposeHandler);
