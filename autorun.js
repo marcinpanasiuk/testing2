1bdd0b842b5ebb780452fa5d8c29369551179c42
marcinpanasiuk <93267852+marcinpanasiuk@users.noreply.github.com>
2025-05-16 07:44:16 +0200

    Update autorun.js

--- autorun.js
+++ autorun.js
@@ -1,14 +1,19 @@
 Office.onReady();
 
-function onNewMessageComposeHandler(event) {
-    var result = "no result";
-    var startTime = performance.now();
-    Office.auth.getAuthContext()
-        .then(function (t) { result = `Success: ${t.userObjectId}`; })
-        .catch(function (e) { result = `Fail: ${e.message}`; })
-        .finally(function () { 
-            var endTime = performance.now();
-            Office.context.mailbox.item.body.setSignatureAsync(`${result}, it took ${Math.round(endTime - startTime)} ms`, { coercionType: "html" }, function () { event.completed(); }) });
+/** inserts a signature automatically when a message is composed or a recipient/sender is changed */
+function insertSignature(event) {
+  Office.context.mailbox.item.body.setSignatureAsync(`
+    <table">
+      <tbody>
+        <tr>
+          <td style="text-align: center">Repro</td>
+        </tr>  
+      </tbody>
+    </table>        
+  `, { coercionType: "html" }, function (asyncResult) { 
+      event.completed(); 
+    });
+  });
 }
 
-Office.actions?.associate("onNewMessageComposeHandler", onNewMessageComposeHandler);
+Office.actions?.associate("insertSignature", insertSignature);
