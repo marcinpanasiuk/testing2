a7cac9c268d0bdce79b22c9497df81b1f3dee4f8
marcinpanasiuk <93267852+marcinpanasiuk@users.noreply.github.com>
2025-05-16 08:22:55 +0200

    Update autorun.js

--- autorun.js
+++ autorun.js
@@ -3,13 +3,13 @@ Office.onReady();
 /** inserts a signature automatically when a message is composed or a recipient/sender is changed */
 function insertSignature(event) {
     Office.context.mailbox.item.body.setSignatureAsync(`
-    <table>
-      <tbody>
+<table>
+    <tbody>
         <tr>
-          <td style="text-align: center">Repro</td>
-        </tr>  
-      </tbody>
-    </table>        
+            <td style="text-align: center">Repro</td>
+        </tr>
+    </tbody>
+</table>
   `, { coercionType: "html" }, function (asyncResult) {
         event.completed();
     });
